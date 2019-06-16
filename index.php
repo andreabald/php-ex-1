3365876437dc1b0f4ae3889da90704f742abd453
Andrea Baldocchi <andrea.baldocchi21@gmail.com>
2019-06-16 18:49:39 +0200

    fine

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- Creare una variabile con del testo (es: lorem ipsum). Leggere dal parametro GET un parola da censurare e stampare a schermo un paragrafo contenente il testo con 3 * (3 caratteri di asterisco) al posto della badword. -->
+
+<?php
+
+  $stringa = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+  echo $stringa."<br>";
+  echo "<br>";
+
+  $badword = $_GET["badword"];
+
+  $end_txt = str_ireplace($badword, "***", $stringa);
+
+  echo "testo sostituito<br>";
+  echo $end_txt;
+
+ ?>
